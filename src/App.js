a6448e8ad935f567080c86f893d481c79683da2e
akramovasadbek <fifaandothergames01@gmail.com>
2023-02-21 19:39:43 +0500

    dolban bu yusufxonjonxon

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 
-
+import Odilake from "./OdilAkhmedov";
 function App() {
   return (
     <div className="App">
-      
+      <Odilake />
     </div>
   );
 }
--- src/OdilAkhmedov.jsx
+++ src/OdilAkhmedov.jsx
@@ -23,3 +23,4 @@ function Odilake(props) {
         </div>
     )
 }
+export default Odilake;
